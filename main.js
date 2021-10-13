5e6f131ee7e2f191fa1a6ac023671d6abde8e808
Nimrod <nimrod.chitayi@gmail.com>
2021-10-13 23:30:41 +0300

    Added more files to the project

---
+++ main.js
@@ -0,0 +1,16 @@
+// Create a calculate function
+
+function calculate() {
+    var p = 0;
+    var y = 0;
+    var r = 0;
+    var simpleInterest = 0.0;
+
+    p = parseInt(document.getElementById("principal").value);
+    y = parseInt(document.getElementById("years").value);
+    r = parseFloat(document.getElementById("rate").value);
+
+    simpleInterest = parseFloat(p * y * r)/100;
+
+    document.getElementById("result").innerHTML = simpleInterest.toLocaleString('en');
+}
